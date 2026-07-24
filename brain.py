a3cc1378a0402a58a93a0417c05ffea22fc6cbb9
NIKLLIS <bestdelar4@gmail.com>
2026-07-25 00:26:32 +0300

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,1 +1,19 @@
-.
+class Brain:
+
+    def __init__(self):
+        self.username = "سيف"
+
+    def think(self, text):
+
+        text = text.lower()
+
+        if "ازيك" in text:
+            return "أنا بخير يا سيف."
+
+        if "اسمك" in text:
+            return "اسمي NIKLLIS."
+
+        if "بحبك" in text:
+            return "وأنا سعيد إني أساعدك يا سيف."
+
+        return None
