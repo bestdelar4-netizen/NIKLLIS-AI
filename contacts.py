ef99d5e9874d498e155e43c546a0aa5eb1c7a8fd
NIKLLIS <bestdelar4@gmail.com>
2026-07-25 23:57:51 +0300

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,1 +1,33 @@
-.
+"""
+NIKLLIS Contacts
+"""
+
+import json
+import os
+
+
+class Contacts:
+
+    def __init__(self):
+        self.file = "contacts.json"
+
+        if os.path.exists(self.file):
+            with open(self.file, "r", encoding="utf-8") as f:
+                self.contacts = json.load(f)
+        else:
+            self.contacts = {
+                "محمد": "محمد",
+                "احمد": "احمد",
+                "ماما": "ماما",
+                "بابا": "بابا"
+            }
+
+    def search(self, name):
+
+        name = name.strip()
+
+        for contact in self.contacts:
+            if name in contact:
+                return self.contacts[contact]
+
+        return None
