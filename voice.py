af90872f0bf205e8db06670474a2e3420c191090
NIKLLIS <bestdelar4@gmail.com>
2026-07-25 23:30:53 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,1 +1,23 @@
-.
+"""
+NIKLLIS Voice
+"""
+
+import pyttsx3
+
+
+class Voice:
+
+    def __init__(self):
+
+        self.engine = pyttsx3.init()
+
+        self.engine.setProperty("rate", 170)
+        self.engine.setProperty("volume", 1.0)
+
+    def speak(self, text):
+
+        print(f"NIKLLIS: {text}")
+
+        self.engine.say(text)
+
+        self.engine.runAndWait()
